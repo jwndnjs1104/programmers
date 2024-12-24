8a30aeb893caa536282b256c51c4034e471727ca
Juwon Jung <jwndnjs1104@gmail.com>
2024-12-24 21:02:42 +0900

    [level 1] Title: 소수 찾기, Time: 187.92 ms, Memory: 53.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        for(int i=2; i<=n; i++){
+            boolean isPrime = true;
+            for(int j=2; j*j<=i; j++){ //소수인지 체크
+                if(i%j==0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if(isPrime) answer++;
+        }
+        return answer;
+    }
+}
